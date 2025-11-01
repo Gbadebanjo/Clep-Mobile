1bb921a59eb7cfde708e84e80e2c4c0dd370e52a
Mr.B <erhimuebru87@gmail.com>
2025-11-01 13:37:33 +0100

    user details screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,7 @@
 import SliderComp from '@/components/Home/slider-comp';
+import BestSellers from '@/components/Products/BestSellers/best-sellers';
+import FashionShowcase from '@/components/Products/FashionShowcase';
+import FeaturedProducts from '@/components/Products/FeaturedProducts';
 import { ThemedView } from '@/components/ThemedView';
 import { ScrollView } from 'react-native';
 
@@ -7,9 +10,9 @@ export default function HomeScreen() {
     <ScrollView>
       <ThemedView>
         <SliderComp />
-        {/* <FeaturedProducts />
+        <FeaturedProducts />
         <FashionShowcase />
-         <BestSellers /> */}
+         <BestSellers />
       </ThemedView>
     </ScrollView>
   );
