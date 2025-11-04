d488535d0bea86ab0e05e3524eaebe8d13215072
Oluwagbogo <oluwagbogoadebanjo@gmail.com>
2025-11-04 19:37:16 +0100

    Fix:clean up

--- components/Products/FashionShowcase/index.tsx
+++ components/Products/FashionShowcase/index.tsx
@@ -5,14 +5,6 @@ import React from 'react';
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import FashionShowStyles from './style';
 
-// const categories = [
-//   { id: 1, name: 'Dresses', image: require('@/assets/images/categories/dress.webp'), link: '/product-category/dresses', gradient: ['#ec4899', '#8b5cf6'] },
-//   { id: 2, name: 'Bags', image: require('@/assets/images/categories/bags.webp'), link: '/product-category/bags', gradient: ['#f59e0b', '#ea580c'] },
-//   { id: 3, name: 'Accessories', image: require('@/assets/images/categories/accessories.webp'), link: '/product-category/accessories', gradient: ['#3b82f6', '#6366f1'] },
-//   { id: 4, name: 'Shoes', image: require('@/assets/images/categories/shoes.webp'), link: '/product-category/shoes', gradient: ['#10b981', '#14b8a6'] },
-//   { id: 5, name: "Men's Wear", image: require('@/assets/images/categories/men-wear.webp'), link: '/product-category/mens-wear', gradient: ['#374151', '#111827'] },
-// ];
-
 const categories = [
   {
     id: 1,
